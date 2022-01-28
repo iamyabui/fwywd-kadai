edba04cf412608ac3f8aac6bba0a3103f0667796
Erika Yabui <yerika.1127@gmail.com>
2022-01-28 23:33:49 +0900

    create json file and modify components

--- components/SkillGraph.js
+++ components/SkillGraph.js
@@ -1,34 +1,21 @@
+import Data from "../src/data.json";
+
 export default function SkillGraph() {
+  const items = Data.skillGraph;
+
   return (
     <div className="bg-white rounded mt-6 pt-4 pb-4 pr-8 pl-8">
-      <p className="text-origin-green">AWS</p>
-      <div className="flex items-center space-x-3">
-        <div className="w-full h-3 bg-progress-color1 rounded">
-          <div className="w-1/2 h-3 bg-progress-color2 rounded"></div>
+      {items.map((data, index) => (
+        <div key={index}>
+          <p className="text-origin-green">{data.skill}</p>
+          <div className="flex items-center space-x-3">
+            <div className="w-full h-3 bg-progress-color1 rounded">
+              <div className="w-1/2 h-3 bg-progress-color2 rounded"></div>
+            </div>
+            <p className="text-origin-green">{data.percentage}%</p>
+          </div>
         </div>
-        <p className="text-origin-green">50%</p>
-      </div>
-      <p className="text-origin-green">React</p>
-      <div className="flex items-center space-x-3">
-        <div className="w-full h-3 bg-progress-color1 rounded">
-          <div className="w-1/2 h-3 bg-progress-color2 rounded"></div>
-        </div>
-        <p className="text-origin-green">50%</p>
-      </div>
-      <p className="text-origin-green">Next.js</p>
-      <div className="flex items-center space-x-3">
-        <div className="w-full h-3 bg-progress-color1 rounded">
-          <div className="w-1/2 h-3 bg-progress-color2 rounded"></div>
-        </div>
-        <p className="text-origin-green">50%</p>
-      </div>
-      <p className="text-origin-green">コミュニケーション</p>
-      <div className="flex items-center space-x-3">
-        <div className="w-full h-3 bg-progress-color1 rounded">
-          <div className="w-1/2 h-3 bg-progress-color2 rounded"></div>
-        </div>
-        <p className="text-origin-green">50%</p>
-      </div>
+      ))}
     </div>
   );
 }
